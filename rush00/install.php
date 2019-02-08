ca2ae0bc45341d87e60d8519ac5e8bfbceee0ad6
Igor VOLOSHIN <ivoloshi@e1r11p3.unit.ua>
2019-02-08 17:21:39 +0200

    del

--- rush00/install.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	exec("~/Library/Containers/MAMP/mysql/bin/mysql -u root -p5956861 < shop.sql");
-	$conn = mysqli_connect('localhost', 'root', '5956861', 'shop');
-
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-?>
